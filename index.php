fc5531b21a4200252164953c17404c36501439e4
Aditya <adityaarrofi806@gmail.com>
2025-09-30 23:37:18 +0700

    progress dashboard

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 get_header();
 ?>
 
-<main class="max-w-6xl mx-auto my-12 text-white">
+<main class="max-w-6xl mx-auto my-12 px-4 sm:px-6 lg:px-8 text-white">
 
     <!-- hero section -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 items-center">
-        <div class="mx-6">
+        <div class="mx-4 sm:mx-6 md:mx-6">
             <h1 class="text-5xl md:text-5xl lg:text-7xl font-bold mb-8">Solusi Jual Beli Aset 3D & Render</h1>
             <p class="text-lg font-light md:text-xl lg:text-2xl">Jual, beli, dan temukan aset 3D sesuai kebutuhan Anda.
                 layanan
@@ -17,7 +17,7 @@ get_header();
                 <div class="grid grid-cols-1 grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
                     <div>
                         <a href=""
-                            class="relative py-4 mr-2 flex w-full items-center justify-center overflow-hidden rounded-xl">
+                            class="relative py-4 mr-2 flex w-full items-center justify-center overflow-hidden rounded-md">
 
                             <div
                                 class="absolute inset-0 bg-gradient-to-r from-[#8000FF] to-[#FF007F] transition-opacity duration-300 hover:opacity-10">
@@ -34,10 +34,10 @@ get_header();
                     </div>
                     <div>
                         <a href="" class="relative block ml-4 w-full py-4 items-center flex justify-center">
-                            <div class="absolute inset-0  rounded-xl bg-gradient-to-r from-[#8000FF] to-[#FF007F]">
+                            <div class="absolute inset-0  rounded-md bg-gradient-to-r from-[#8000FF] to-[#FF007F]">
                             </div>
 
-                            <div class="absolute inset-[2px]  rounded-xl bg-[#212121] transition hover:opacity-10">
+                            <div class="absolute inset-[2px]  rounded-md bg-[#212121] transition hover:opacity-10">
                             </div>
 
                             <span class=" relative text-lg font-semibold ">Mulai Render</span>
@@ -71,10 +71,10 @@ get_header();
 
     <!-- asset section -->
 
-    <div class="mt-20">
+    <div class="mt-20 mb-12">
         <div class="my-6">
-            <h3 class="font-bold text-3xl">Asset Yang Sedang Tren</h3>
-            <p class="font-light">Lihatlah koleksi tren mingguan kami yang diperbarui.</p>
+            <h3 class="font-bold text-3xl mb-4">Asset Yang Sedang Tren</h3>
+            <p class="font-light text-xl">Lihatlah koleksi tren mingguan kami yang diperbarui.</p>
         </div>
     </div>
 
@@ -162,14 +162,14 @@ get_header();
 
     <!-- kreator section -->
 
-    <div class="mt-20">
+    <div class="mt-20 mb-12">
         <div class="my-6">
-            <h3 class="font-bold text-3xl">Kreator Teratas</h3>
-            <p class="font-light">Lihat Kreator Berperingkat Teratas di Marketplace</p>
+            <h3 class="font-bold text-3xl mb-4">Kreator Teratas</h3>
+            <p class="font-light text-xl">Lihat Kreator Berperingkat Teratas di Marketplace</p>
         </div>
     </div>
 
-    <div class="grid grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-5">
         <div class="rounded-xl bg-[#2E2E2E]">
             <div class="flex justify-center">
                 <div class="m-5">
@@ -213,7 +213,7 @@ get_header();
         </div>
     </div>
 
-    <div class="grid grid-cols-5 gap-3 mt-5">
+    <div class="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-6 mt-5">
         <div class="rounded-xl bg-[#2E2E2E]">
             <div class="flex justify-center">
                 <div class="m-5">
@@ -287,7 +287,7 @@ get_header();
 
         <div class="rounded-xl bg-[#2E2E2E]">
             <div class="flex justify-center">
-                <div class="m-5">
+                <div class="m-5 ">
                     <img src="https://placehold.co/100x100" alt="" class="rounded-full" />
                 </div>
             </div>
@@ -358,11 +358,91 @@ get_header();
 
     </div>
 
+    <div class="mt-20 flex flex-col md:flex-row items-center justify-between w-full mb-12">
+
+        <div class="mb-4 md:mb-0 text-start md:text-left flex-1">
+            <h3 class="font-bold text-3xl mb-2">Temukan Lebih Banyak Aset</h3>
+            <p class="font-light text-xl">Jelajahi aset yang sedang tren.</p>
+        </div>
 
 
+        <div class="w-full md:w-auto flex justify-center md:justify-end">
+            <a href="" class="relative block w-full m-4 md:w-auto p-3">
+                <div class="absolute inset-0 rounded-md bg-gradient-to-r from-[#8000FF] to-[#FF007F]"></div>
+                <div class="absolute inset-[2px] rounded-md bg-[#212121] transition hover:opacity-10"></div>
+                <span class="relative text-sm font-semibold flex justify-center">Lihat Aset Lainnya</span>
+            </a>
+        </div>
+    </div>
 
+
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-0">
+        <div class="w-full mx-auto bg-black rounded-2xl shadow-lg overflow-hidden my-2">
+            <div>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/home.png" alt=""
+                    class="w-full h-80  object-cover rounded-t-2xl">
+            </div>
+
+            <div class="py-3  bg-[#2E2E2E] px-6 w-full rounded-b-lg">
+                <h1 class="text-3xl font-bold mb-3">Steampunk Girl</h1>
+                <div class="flex justify-start">
+                    <img src="https://placehold.co/30x30" alt="" class="mr-3 rounded-full">
+                    <p>Aditya Arrofi</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full mx-auto bg-black rounded-2xl shadow-lg overflow-hidden my-2">
+            <div>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/home.png" alt=""
+                    class="w-full h-80  object-cover rounded-t-2xl">
+            </div>
+
+            <div class="py-3  bg-[#2E2E2E] px-6 w-full rounded-b-lg">
+                <h1 class="text-3xl font-bold mb-3">Steampunk Girl</h1>
+                <div class="flex justify-start">
+                    <img src="https://placehold.co/30x30" alt="" class="mr-3 rounded-full">
+                    <p>Aditya Arrofi</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full mx-auto bg-black rounded-2xl shadow-lg overflow-hidden my-2">
+            <div>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/home.png" alt=""
+                    class="w-full h-80  object-cover rounded-t-2xl">
+            </div>
+
+            <div class="py-3  bg-[#2E2E2E] px-6 w-full rounded-b-lg">
+                <h1 class="text-3xl font-bold mb-3">Steampunk Girl</h1>
+                <div class="flex justify-start">
+                    <img src="https://placehold.co/30x30" alt="" class="mr-3 rounded-full">
+                    <p>Aditya Arrofi</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </main>
+
+
+<div class=" relative w-[1500px] h-[650px] bg-cover bg-center"
+    style="background-image: url('https://placehold.co/1500x650/png');">
+
+
+    <div class="justify-between absolute bottom-56 ">halo</div>
+
+    <div class=" w-[1500px] flex justify-between absolute bottom-36 ">
+        <div class="text-5xl font-bold">Steampunk Hammer</div>
+        <div class="text-5xl font-bold">halo</div>
+    </div>
+</div>
+
+
+
+
+
+
 <?php
 // Panggil footer.php
 get_footer();
